372616509a37b4f2d4f5818794cd445606b16771
pouyax <75066687+with-pouyax@users.noreply.github.com>
2025-09-12 16:09:07 +0200

    Improve comments in bigint addition operator
    Updated comments in the operator+ function for clarity.

--- 42exam05-main 2/Tests/level1/bigint/bigint.cpp
+++ 42exam05-main 2/Tests/level1/bigint/bigint.cpp
@@ -53,40 +53,39 @@ const std::string	bigint::getNum() const
 }
 
 // addition
-bigint	bigint::operator+(const bigint& other)const
+bigint	bigint::operator+ (const bigint& other)const
 {
 	std::string leftNumber = this->_number;
 	std::string rightNumber = other._number;
 
 
-	int leftIndex = leftNumber.size() - 1;
+	int leftIndex = leftNumber.size() - 1;              // -1 because we want to start from the last digit
 	int rightIndex = rightNumber.size() - 1;
 
 	std::string result;
-	result.reserve(leftIndex > rightIndex ? leftIndex + 1 : rightIndex + 1);
+	result.reserve(leftIndex > rightIndex ? leftIndex + 1 : rightIndex + 1);   we reserve for result in the size of the longest number + 1 for the carry
 
 	int carry = 0;
-	while (leftIndex >= 0 || rightIndex >= 0 || carry)
+	while (leftIndex >= 0 || rightIndex >= 0 || carry) // we continue while there are digits to add or a carry
 	{
-		int leftDigit = leftIndex >= 0 ? leftNumber[leftIndex] - '0' : 0;
-		int rightDigit = rightIndex >= 0 ? rightNumber[rightIndex] - '0' : 0;
+		int leftDigit = leftIndex >= 0 ? leftNumber[leftIndex] - '0' : 0;      // if there are a digits in rightNumber, we store the digit in leftDigit, otherwise we store 0
+		int rightDigit = rightIndex >= 0 ? rightNumber[rightIndex] - '0' : 0;  //
 		
-		int digitSum = leftDigit + rightDigit + carry;
+		int digitSum = leftDigit + rightDigit + carry;                         // we add the most left digits from leftNumber and rightNumber and the carry
 
-		result.push_back((digitSum % 10) + '0');
+		result.push_back((digitSum % 10) + '0');                       //we convert the most right digit of the digitSum to a char and add it to the result
 
-		carry = digitSum / 10;
+		carry = digitSum / 10;                                         // we store the most left digit of the digitSum in the carry variable
 
-		leftIndex--;
-		rightIndex--;
+		leftIndex--;                                                // we move to the next digit (from right to left)
+		rightIndex--;                                                
 	}
 
-	std::reverse(result.begin(), result.end());
+	std::reverse(result.begin(), result.end());                   // we reverse the result to get the correct order of the digits
 
-	bigint resultBigint(result);
+	bigint resultBigint(result);                                  // we create a new bigint with the result using the bigint(std::string n) constructor.
 	return resultBigint;
 }
-
 bigint	bigint::operator+=(const bigint& other)
 {
 	*this = *this + other;
