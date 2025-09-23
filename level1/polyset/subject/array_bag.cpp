10afad99d63f38b7e378f02114ff8644cdae7a32
Pouya Ghajardavalou <pghajard@c3r8p6.42vienna.com>
2025-09-23 18:17:21 +0200

    add some cmts

--- level1/polyset/subject/array_bag.cpp
+++ level1/polyset/subject/array_bag.cpp
@@ -7,15 +7,15 @@ array_bag::array_bag() {
 }
 
 array_bag::array_bag(const array_bag &src) {
-  size = src.size;
-  data = new int[size];
-  for (int i = 0; i < size; i++) {
+  size = src.size;                                  // copy the size of the array
+  data = new int[size];                             // allocate memory for the array
+  for (int i = 0; i < size; i++) {                  // copy the elements of the array
 	data[i] = src.data[i];
   }
 }
 
 array_bag &array_bag::operator=(const array_bag &src) {
-	if (this != &src) {
+	if (this != &src) {                 // if the array is not the same and exists we delete it and create a new one and copy the elements to it
 		if (data != nullptr) {
 			delete[] data;
 			data = nullptr;
@@ -30,31 +30,31 @@ array_bag &array_bag::operator=(const array_bag &src) {
 }
 
 array_bag::~array_bag() {
-	if (data != nullptr) {
+	if (data != nullptr) {                      // if the array exists we delete it
 		delete[] data;
 		data = nullptr;
 	}
 }
 
 void array_bag::insert(int item) {
-	int *new_data = new int[size + 1];
-	for (int i = 0; i < size; i++) {
+	int *new_data = new int[size + 1];             // we create a new array with one more element than the current one
+	for (int i = 0; i < size; i++) {               // we loop through the current array and copy the elements to the new array
 		new_data[i] = data[i];
 	}
-	new_data[size] = item;
-	if (data != nullptr) {
+	new_data[size] = item;                         // we add the new element at the end of the array
+	if (data != nullptr) {                         // if the current array exists we delete it
 		delete[] data;
 	}
-	data = new_data;
-	size++;
+	data = new_data;                               // we assign the new array to the current array
+	size++;                                        // we increment the size of the array
 }
 
-void array_bag::insert(int *items, int count) {
-	int *new_data = new int[size + count];
-	for (int i = 0; i < size; i++) {
-		new_data[i] = data[i];
+void array_bag::insert(int *items, int count) {    // we receive an array of integers and the number of elements in the array
+	int *new_data = new int[size + count];         // we create a new array with size of the current array + the number of elements in the array we receive
+	for (int i = 0; i < size; i++) {               // we loop through the current array and copy the elements to the new array
+		new_data[i] = data[i];                     // we copy the elements of the current array to the new array
 	}
-	for (int i = 0; i < count; i++) {
+	for (int i = 0; i < count; i++) {              
 		new_data[size + i] = items[i];
 	}
 	if (data != nullptr) {
@@ -64,14 +64,14 @@ void array_bag::insert(int *items, int count) {
 	size += count;
 }
 
-void array_bag::print() const {
-	for (int i = 0; i < size; i++) {
+void array_bag::print() const {                     // we print the elements of the array
+	for (int i = 0; i < size; i++) {               
 		std::cout << data[i] << " ";
 	}
 	std::cout << std::endl;
 }
 
-void array_bag::clear() {
+void array_bag::clear() {                           // we clear the array and set the size to 0
 	if (data != nullptr) {
 		delete[] data;
 		data = nullptr;
--- level1/polyset/subject/bag.hpp
+++ level1/polyset/subject/bag.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-class bag {
+class bag {                                         // it is our abstract class that will be inherited by our other classes
 public:
-	virtual void insert (int) = 0;
+	virtual void insert (int) = 0;                  // 
 	virtual void insert (int *, int) = 0;
 	virtual void print() const = 0;
 	virtual void clear() = 0;
